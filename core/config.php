6e8d90f4f1951ccef9efcbcc1657b3b9421d9673
eastcause <105230470+eastcause@users.noreply.github.com>
2022-10-09 20:46:30 +0200

    Update config.php

--- core/config.php
+++ core/config.php
@@ -3,8 +3,3 @@ const DB_HOST = "localhost";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_DATABASE = "aplikacja";
-const HOST = "localhost";
-const DIR = "php_blog";
-
-// HOST + / + DIR
-// localhost/php_blog/
